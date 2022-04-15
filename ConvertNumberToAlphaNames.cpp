d8be82fe0ef50ef341577c170783f4d6d285bd13
Darren <93838192+AFunProgrammer@users.noreply.github.com>
2022-04-14 21:14:38 -0700

    Created CGetNumber class and "quit" to exit
    You now need to use "quit" to exit the execution of the program
    Using the map container for number groups (powers of 1000) and digit names
    Switching to using the CGetNumber class object with static functions for getting specific names and group names
    Code cleanup and reducing the code bloat by using more c++ type syntax

--- ConvertNumberToAlphaNames.cpp
+++ ConvertNumberToAlphaNames.cpp
@@ -9,18 +9,169 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <deque>
 #include <iostream>
 #include <stdlib.h>
-
+#include <map>
 using namespace std;
 
 string removeNonNumberCharacters(const string RemoveNonNumbers);
 bool isInScientificNotation(const string CheckForScientificNotation);
 string convertFromScientificNotation(const string ScientificNotation);
 string convertToScientificNotation(const string Value);
-string getOddTensValues(int Value);
-string getOnesString(int Value, bool Tens);
-string getUnitsString(int DigitsPlace, int Value);
 bool isTensDigit(int DigitsPlace);
 string ConvertNumberToWords(string Value);
+
+class CGetNumber
+{
+    private:
+/* https://simple.wikipedia.org/wiki/Names_for_large_numbers 
+    1 	Ten 	Ten 	Ten 	da 	Deca-
+    2 	Hundred 	Hundred 	Hundred 	h 	Hecto-
+    3 	Thousand 	Thousand 	Thousand 	K 	Kilo-
+    4 	Ten thousand 	Ten thousand 	Ten thousand 	my (Now obsolete) 	Myria- (Now obsolete)
+    5 	Hundred thousand 	Hundred thousand 	Hundred thousand 		
+    6 	Million 	Million 	Million 	M 	Mega-
+    9 	Billion 	Milliard 	Thousand million 	G 	Giga-
+    12 	Trillion 	Billion 	Billion 	T 	Tera-
+    15 	Quadrillion 	Billiard 	Thousand billion 	P 	Peta- 
+    18 	Quintillion 	Trillion 	Trillion 	E 	Exa-
+    21 	Sextillion 	Trilliard 	Thousand trillion 	Z 	Zetta-
+    24 	Septillion 	Quadrillion 	Quadrillion 	Y 	Yotta-
+    27 	Octillion 	Quadrilliard 	Thousand quadrillion
+    30 	Nonillion 	Quintilion 	Quintillion
+    33 	Decillion 	Quintilliard 	Thousand quintillion
+    36 	Undecillion 	Sextillion 	Sextillion
+    39 	Duodecillion 	Sextilliard 	Thousand sextillion
+    42 	Tredecillion 	Septillion 	Septillion
+    45 	Quattuordecillion 	Septilliard 	Thousand septillion
+    48 	Quindecillion 	Octillion 	Octillion
+    51 	Sexdecillion 	Octilliard 	Thousand octillion
+    54 	Septendecillion 	Nonillion 	Nonillion
+    57 	Octodecillion 	Nonilliard 	Thousand nonillion
+    60 	Novemdecillion 	Decillion 	Decillion
+    63	Vigintillion	Decilliard	Thousand decillion
+    66	Unvigintillion	Undecillion	Undecillion
+    69	Duovigintillion	Undecilliard	Thousand undecillion
+    72	Trevigintillion	Duodecillion	Duodecillion
+    75	Quattuorvigintillion	Duodecilliard	Thousand duodecillion
+    78	Quinvigintillion	Tredecillion	Tredecillion
+    81	Sexvigintillion	Tredecilliard	Thousand tredecillion
+    84	Septenvigintillion	Quattuordecillion	Quattuordecillion
+    87	Octovigintillion	Quattuordecilliard	Thousand quattuordecillion
+    90	Novemvigintillion	Quindecillion	Quindecillion
+    93	Trigintillion	Quindecilliard	Thousand quindecillion
+    96	Untrigintillion	Sexdecillion	Sexdecillion
+    99	Duotrigintillion	Sexdecilliard	Thousand sexdecillion
+    100	Googol	Googol	Googol    
+    102 Tretrigintillion 	Septendecillion 	Septendecillion
+    105 Quattuortrigintillion 	Septendecilliard 	Thousand septedecillion
+    108 Quintrigintillion 	Octodecillion 	Octodecillion
+    111 Sextrigintillion 	Octodecilliard 	Thousand octodecillion
+    114 Septentrigintillion 	Novemdecillion 	Novemdecillion
+    117 Octotrigintillion 	Novemdecilliard 	Thousand novemdecillion
+    120 Novemtrigintillion 	Vigintillion 	Vigintillion     
+    //https://en.wikipedia.org/wiki/Names_of_large_numbers
+    123	Quadragintillion	Thousand vigintillion	Vigintilliard
+*/    
+    //map<10^x,name>
+    static map<int,string> m_NumberGroups;
+    static map<int, tuple<string,string,string>> m_DigitNames;
+    
+    public:
+    
+    CGetNumber(){}
+    
+    static string getUnitsString(int Place, int Value)
+    {
+        string unit = "";
+    
+        if ( (Place + 1) % 3 == 0 )
+        {
+            unit = (Value == 0 ? "" : string("hundred"));
+        }
+        else
+        {
+            unit = m_NumberGroups[Place];
+        }
+    
+        return unit;
+    }
+
+    static string getOnesValue(int Value)
+    {
+        return get<0>(m_DigitNames[Value]);
+    }
+    
+    static string getTeensValue(int Value)
+    {
+        return get<1>(m_DigitNames[Value]);   
+    }
+    
+    static string getTensValue(int Value)
+    {
+        return get<2>(m_DigitNames[Value]);
+    }
+};
+
+map<int,string> CGetNumber::m_NumberGroups
+{
+    {3  , "thousand"},
+    {6  , "million"},
+    {9  , "billion"},
+    {12 , "trillion"},
+    {15 , "quadrillion"},
+    {18 , "quintillion"},
+    {21 , "sextillion"},
+    {24 , "septillion"},
+    {27 , "octillion"},
+    {30 , "nonillion"},
+    {33 , "decillion"},
+    {36 , "undecillion"},
+    {39 , "duodecillion"},
+    {42 , "tredecillion"},
+    {45 , "quattuordecillion"},
+    {48 , "quindecillion"},
+    {51 , "sexdecillion"},
+    {54 , "septendecillion"},
+    {57 , "octodecillion"},
+    {60 , "novemdecillion"},
+    {63 , "vigintillion"},
+    {66 , "unvigintillion"},
+    {69 , "duovigintillion"},
+    {72 , "trevigintillion"},
+    {75 , "quattuorvigintillion"},
+    {78 , "quinvigintillion"},
+    {81 , "sexvigintillion"},
+    {84 , "septenvigintillion"},
+    {87 , "octovigintillion"},
+    {90 , "novemvigintillion"},
+    {93 , "trigintillion"},
+    {96 , "untrigintillion"},
+    {99 , "duotrigintillion"},
+    {100, "googol"},
+    {102, "tretrigintillion"},
+    {105, "quattuortrigintillio"},
+    {108, "quintrigintillion"},
+    {111, "sextrigintillion"},
+    {114, "septentrigintillion"},
+    {117, "octotrigintillion"},
+    {120, "novemtrigintillion"},
+    {123, "quadragintillion"}
+};
+
+map<int, tuple<string,string,string>> CGetNumber::m_DigitNames
+{
+    {0, make_tuple("",     "ten",      "")},
+    {1, make_tuple("one",  "eleven",   "")},
+    {2, make_tuple("two",  "twelve",   "twenty")},
+    {3, make_tuple("three","thirteen", "thirty")},
+    {4, make_tuple("four", "fourteen", "fourty")},
+    {5, make_tuple("five", "fifteen",  "fifty")},
+    {6, make_tuple("six",  "sixteen",  "sixty")},
+    {7, make_tuple("seven","seventeen","seventy")},
+    {8, make_tuple("eight","eighteen", "eighty")},
+    {9, make_tuple("nine", "nineteen", "ninety")},    
+};
+
+CGetNumber g_GetNumber;
 
 bool isInScientificNotation(const string CheckForScientificNotation)
 {
@@ -131,248 +282,6 @@ string removeNonNumberCharacters(const string RemoveNonNumbers)
 }
 
 
-string getOddTensValues(int Value)
-{
-    string out = "";
-    
-    switch(Value)
-    {
-        case 0:
-            out = string("ten");
-            break;
-        case 1:
-            out = string("eleven");
-            break;
-        case 2:
-            out = string("twelve");
-            break;
-        case 3:
-            out = string("thirteen");
-            break;
-        case 4:
-            out = string("fourteen");
-            break;
-        case 5:
-            out = string("fifteen");
-            break;
-        case 6:
-            out = string("sixteen");
-            break;
-        case 7:
-            out = string("seventeen");
-            break;
-        case 8:
-            out = string("eighteen");
-            break;
-        case 9:
-            out = string("nineteen");
-            break;
-    }
-    
-    return out;   
-}
-
-string getOnesString(int Value, bool Tens)
-{
-    string out = "";
-
-    switch(Value)
-    {
-        case 0:
-            //cout << (idigit == 2 ? "ten" : "zero" );
-            break;
-        case 1:
-            out = string(Tens ? "" : "one");
-            break;
-        case 2:
-            out = string(Tens ? "twenty" : "two" );
-            break;
-        case 3:
-            out = string(Tens ? "thirty" : "three" );
-            break;
-        case 4:
-            out = string(Tens ? "fourty" : "four" );
-            break;
-        case 5:
-            out = string(Tens ? "fifty" : "five" );
-            break;
-        case 6:
-            out = string(Tens ? "sixty" : "six" );
-            break;
-        case 7:
-            out = string(Tens ? "seventy" : "seven" );
-            break;
-        case 8:
-            out = string(Tens ? "eighty" : "eight" );
-            break;
-        case 9:
-            out = string(Tens ? "ninety" : "nine" );
-            break;
-    }
-    
-    return out;
-}
-
-string getUnitsString(int DigitsPlace, int Value)
-{
-    string out = "";
-
-    if ( (DigitsPlace + 1) % 3 == 0 )
-    {
-        out = (Value == 0 ? "" : string("hundred"));
-    }
-
-    switch ( DigitsPlace )
-    {
-        case 3:
-            out = string("thousand");
-            break;
-        case 6:
-            out = string("million");
-            break;
-        case 9:
-            out = string("billion");
-            break;
-        case 12:
-            out = string("trillion");
-            break;
-        case 15:
-            out = string("quadrillion");
-            break;
-        case 18:
-            out = string("quintillion");
-            break;
-        case 21:
-            out = string("sextillion");
-            break;
-        case 24:
-            out = string("septillion");
-            break;
-        case 27:
-            out = string("octillion");
-            break;
-        case 30:
-            out = string("nonillion");
-            break;
-        case 33:
-            out = string("decillion");
-            break;
-        case 36:
-            out = string("undecillion");
-            break;
-        case 39:
-            out = string("duodecillion");
-            break;
-        case 42:
-            out = string("tredecillion");
-            break;
-        case 45:
-            out = string("quattuordecillion");
-            break;
-        case 48:
-            out = string("quindecillion");
-            break;
-        case 51:
-            out = string("sexdecillion");
-            break;
-        case 54:
-            out = string("septendecillion");
-            break;
-        case 57:
-            out = string("octodecillion");
-            break;
-        case 60:
-            out = string("novemdecillion");
-            break;
-        case 63:
-            out = string("vigintillion");
-            break;
-        case 66:
-            out = string("unvigintillion");
-            break;
-        case 69:
-            out = string("duovigintillion");
-            break;
-        case 72:
-            out = string("trevigintillion");
-            break;
-        case 75:
-            out = string("quattuorvigintillion");
-            break;
-        case 78:
-            out = string("quinvigintillion");
-            break;
-        case 81:
-            out = string("sexvigintillion");
-            break;    
-        case 84:
-            out = string("septenvigintillion");
-            break;
-        case 87:
-            out = string("octovigintillion");
-            break;
-        case 90:
-            out = string("novemvigintillion");
-            break;
-        case 93:
-            out = string("trigintillion");
-            break;
-        case 96:
-            out = string("untrigintillion");
-            break;
-        case 99:
-            out = string("duotrigintillion");
-            break;
-        case 100:
-            out = string("googol");
-            break;
-    }
-
-    /* https://simple.wikipedia.org/wiki/Names_for_large_numbers 
-    Place
-    1 	Ten 	Ten 	Ten 	da 	Deca-
-    2 	Hundred 	Hundred 	Hundred 	h 	Hecto-
-    3 	Thousand 	Thousand 	Thousand 	K 	Kilo-
-    4 	Ten thousand 	Ten thousand 	Ten thousand 	my (Now obsolete) 	Myria- (Now obsolete)
-    5 	Hundred thousand 	Hundred thousand 	Hundred thousand 		
-    6 	Million 	Million 	Million 	M 	Mega-
-    9 	Billion 	Milliard 	Thousand million 	G 	Giga-
-    12 	Trillion 	Billion 	Billion 	T 	Tera-
-    15 	Quadrillion 	Billiard 	Thousand billion 	P 	Peta- 
-    18 	Quintillion 	Trillion 	Trillion 	E 	Exa-
-    21 	Sextillion 	Trilliard 	Thousand trillion 	Z 	Zetta-
-    24 	Septillion 	Quadrillion 	Quadrillion 	Y 	Yotta-
-    27 	Octillion 	Quadrilliard 	Thousand quadrillion
-    30 	Nonillion 	Quintilion 	Quintillion
-    33 	Decillion 	Quintilliard 	Thousand quintillion
-    36 	Undecillion 	Sextillion 	Sextillion
-    39 	Duodecillion 	Sextilliard 	Thousand sextillion
-    42 	Tredecillion 	Septillion 	Septillion
-    45 	Quattuordecillion 	Septilliard 	Thousand septillion
-    48 	Quindecillion 	Octillion 	Octillion
-    51 	Sexdecillion 	Octilliard 	Thousand octillion
-    54 	Septendecillion 	Nonillion 	Nonillion
-    57 	Octodecillion 	Nonilliard 	Thousand nonillion
-    60 	Novemdecillion 	Decillion 	Decillion
-    63	Vigintillion	Decilliard	Thousand decillion
-    66	Unvigintillion	Undecillion	Undecillion
-    69	Duovigintillion	Undecilliard	Thousand undecillion
-    72	Trevigintillion	Duodecillion	Duodecillion
-    75	Quattuorvigintillion	Duodecilliard	Thousand duodecillion
-    78	Quinvigintillion	Tredecillion	Tredecillion
-    81	Sexvigintillion	Tredecilliard	Thousand tredecillion
-    84	Septenvigintillion	Quattuordecillion	Quattuordecillion
-    87	Octovigintillion	Quattuordecilliard	Thousand quattuordecillion
-    90	Novemvigintillion	Quindecillion	Quindecillion
-    93	Trigintillion	Quindecilliard	Thousand quindecillion
-    96	Untrigintillion	Sexdecillion	Sexdecillion
-    99	Duotrigintillion	Sexdecilliard	Thousand sexdecillion
-    100	Googol	Googol	Googol    
-    */
-
-    return out;
-}
-
 bool isTensDigit(int DigitsPlace)
 {
     if ( (DigitsPlace + 2) % 3 == 0 )
@@ -406,9 +315,16 @@ string ConvertNumberToWords(string Value)
         bool bTens = isTensDigit(i);
         
         if ( i+1 <= iDigit && isTensDigit(i+1) && iPrevNumber == 1 )
-            word = getOddTensValues( iNumber );
+        {
+            word = g_GetNumber.getTeensValue( iNumber );
+        }
         else
-            word = getOnesString( iNumber, bTens );
+        {
+            if ( bTens )
+                word = g_GetNumber.getTensValue( iNumber );
+            else
+                word = g_GetNumber.getOnesValue( iNumber );
+        }
 
         ConvertedToWords += word;
         
@@ -417,7 +333,7 @@ string ConvertNumberToWords(string Value)
 
         if ( bNumberInGroup )
         {
-            word = getUnitsString(i,iNumber);
+            word = g_GetNumber.getUnitsString(i,iNumber);
         
             ConvertedToWords += word;
         
@@ -436,30 +352,40 @@ int main()
 {
     string sCleaned = string("");
     string sInput = string("");
-    cout << "Input a number to convert to it's word value (use 117000 or 1.17e+5 or 1.17e5 for input): ";
-    cin >> sInput;
-    cout << "I Received: " << sInput << endl;
-    sCleaned = removeNonNumberCharacters(sInput);
-    
-    if( sCleaned.length() != sInput.length() )
-    {
-        cout << "The Number after removing extraneous characters: " << sCleaned << endl;
-        sInput = sCleaned;
-    }
-    
-    if ( isInScientificNotation(sInput) )
-    {
-        sInput = convertFromScientificNotation(sInput);
-    }
-    else
-    {
-        cout << "In Scientific Notation: " << convertToScientificNotation(sInput) << endl;
-    }
 
-    cout << "In Decimal Format: " << sInput << endl;
-    cout << "The Number Is Read As: " << endl;
+    cout << "Input a number to convert to it's word value (use 117000 or 1.17e+5 or 1.17e5 for input) [type \"quit\" to end]: ";
+    cin >> sInput;
     
-    cout << ConvertNumberToWords(sInput);
+    while ( sInput != "quit" )
+    {
+        cout << "I Received: " << sInput << endl;
+        sCleaned = removeNonNumberCharacters(sInput);
+        
+        if( sCleaned.length() != sInput.length() )
+        {
+            cout << "The Number after removing extraneous characters: " << sCleaned << endl;
+            sInput = sCleaned;
+        }
+        
+        if ( isInScientificNotation(sInput) )
+        {
+            sInput = convertFromScientificNotation(sInput);
+        }
+        else
+        {
+            cout << "In Computer Scientific Notation: " << convertToScientificNotation(sInput) << endl;
+        }
+    
+        cout << "In Decimal Format: " << sInput << endl;
+        cout << "The Number Is Read As: " << endl;
+        
+        cout << ConvertNumberToWords(sInput) << endl;
+        
+        cout << "------------------------------------------------------------------------" << endl;
+        cout << endl;
+        cout << "Input a number to convert to it's word value (use 117000 or 1.17e+5 or 1.17e5 for input): ";
+        cin >> sInput;
+    }
 
     return 0;
 }
